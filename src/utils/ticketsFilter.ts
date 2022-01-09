2b1671e4ae8910d04d4e7eb16e8aa19da04e3a1b
Maksim <timeshin0485@gmail.com>
2022-01-09 15:01:44 +0300

    edit filtering, prep for tests, install dependecies

--- src/utils/ticketsFilter.ts
+++ src/utils/ticketsFilter.ts
@@ -2,28 +2,42 @@ import { ITickets } from "../redux/reducerTypes/type"
 import { ITicketsFilter } from "../types/types"
 
 const ticketsFilter = (
-        tickets: ITickets[],
-        filterArr: number[],
-        qtyTickets: number,
-        mainFilter: string
-    ): ITicketsFilter => {
+    tickets: ITickets[],
+    filterArr: number[],
+    qtyTickets: number,
+    mainFilter: string
+): ITicketsFilter => {
 
 
-    const filteredTickets = filterArr.length !== 0 ? tickets.filter(ticket => 
-        ticket.segments.every(segment => 
-                filterArr.includes(segment.stops.length)
+    const filteredTickets = (): ITickets[] => {
+        let arr: ITickets[] = []
+
+        if (filterArr.length !== 0) {
+            arr = [...tickets].filter(ticket =>
+                ticket.segments.every(segment =>
+                    filterArr.includes(segment.stops.length)
+                )
             )
-        )
-        :
-        tickets
-        
-    const returnedTickets = mainFilter === "cheapest" ?
-    filteredTickets.slice(0, qtyTickets).sort((a, b) => a.price - b.price) :
-    filteredTickets.slice(0, qtyTickets).sort((a, b) => a.segments[0].duration - b.segments[0].duration)
+        } else {
+            arr = [...tickets]
+        }
+
+        if (mainFilter === "cheapest") {
+            arr.sort((a, b) => a.price - b.price)
+        } else {
+            arr.sort((a, b) => {
+                return (a.segments[0].duration + a.segments[1].duration) - (b.segments[0].duration + b.segments[1].duration)
+            })
+        }
+
+        return arr
+    }
+
+    const returnedTickets = filteredTickets().slice(0, qtyTickets)
 
     return {
         tickets: returnedTickets,
-        filteredTickets
+        filteredTickets: filteredTickets()
     }
 }
 
